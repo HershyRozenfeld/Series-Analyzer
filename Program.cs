0dd3617370c807c971d201e7fc00f5da33b85473
hershy rozenfeld <Y6764057@gmail.com>
2025-05-12 16:10:46 +0300

    the skeleton of the program

--- Program.cs
+++ Program.cs
@@ -11,5 +11,46 @@ namespace project_1
         static void Main(string[] args)
         {
         }
+        static int max(int[] intArr)
+        {
+            int bigest = 0;
+            return bigest;
+        }
+        static int min(int[] intArr)
+        {
+            int smallest = 0;
+            return smallest;
+        }
+        static int sum(int[] intArr)
+        {
+            int sum = 0;
+            return sum;
+        }
+        static int avg(int[] intArr)
+        {
+            int avg = 0;
+            return avg;
+        } 
+        static int count(int[] intArr)
+        {
+            int count = 0;
+            return count;
+        }
+        static int[] ascendingSort(int[] intArr)
+        {
+            int[] sortedArr = new int[intArr.Length];
+            return sortedArr;
+        }
+        static int[] revers(int[] intArr)
+        {
+            int[] reversedArr = new int[intArr.Length];
+            return reversedArr;
+        }
+        static void arrPrinter(int[] intArr)
+        {
+        }
+        static void singleNumPrinter(int[] intArr)
+        {
+        }
     }
 }
