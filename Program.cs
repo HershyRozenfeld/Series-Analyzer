1547edb14a3f3adfb3e7e63e3d7da46e299982fe
hershy rozenfeld <Y6764057@gmail.com>
2025-05-13 17:13:16 +0300

    add the menu of the user & punctions for print

--- Program.cs
+++ Program.cs
@@ -10,11 +10,116 @@ namespace project_1
     {
         static void Main(string[] args)
         {
+            int[] intArr = checkInput(args);
+            bool flage = false;
+            while (!flage)
+            {
+                menuPriter();
+                string input = Console.ReadLine();
+                switch (input)
+                {
+                    case "a":
+                        intArr = checkInput(args);
+                        break;
+                    case "b":
+                        arrPrinter(intArr);
+                        break;
+                    case "c":
+                        arrPrinter(revers(intArr));
+                        break;
+                    case "d":
+                        arrPrinter(ascendingSort(intArr));
+                        break;
+                    case "e":
+                        singleNumPrinter(max(intArr));
+                        break;
+                    case "f":
+                        singleNumPrinter(min(intArr));
+                        break;
+                    case "g":
+                        singleNumPrinter(avg(intArr));
+                        break;
+                    case "h":
+                        singleNumPrinter(count(intArr));
+                        break;
+                    case "i":
+                        singleNumPrinter(sum(intArr));
+                        break;
+                    case "j":
+                        flage = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input. Please try again.");
+                        break;
+                }
+            }
         }
-        static string[] arrInput()
+
+        static int[] checkInput(string[] strArr)  // This method takes a string array as input and converts it to an integer array.
         {
-            //string[] strArr = new string[10];
-            //return strArr;
+            bool flage = false;
+            while(flage == false) 
+            {
+                flage = true;
+                if (strArr.Length >= 3)
+                {
+                    foreach (string str in strArr)
+                    {
+                        foreach (char ch in str)
+                        {
+                            if (!char.IsDigit(ch) && ch != ' ')
+                            {
+                                strArr = getInput();
+                                flage = false;
+                                break;
+                            }
+                        }
+                        if (!flage)
+                        {
+                            break;
+                        }
+                    }
+                }
+                else
+                {
+                    strArr = getInput();
+                    flage = false;
+                }
+            }
+            return convert(strArr);
+        }
+        static string[] getInput()
+        {
+            string[] strArr;
+            Console.WriteLine("Please enter a numerical series (Give a space to separate a number from another number): ");
+            strArr = Console.ReadLine().Split(' ');
+            return strArr;
+        }
+        static int[] convert(string[] strArr)
+        {
+            int[] intArr = new int[strArr.Length];
+            for (int i = 0; i < strArr.Length; i++)
+            {
+                intArr[i] = Convert.ToInt32(strArr[i]);
+            }
+            return intArr;
+        }
+        // So far, the absorption stage of the numerical series.
+
+        static void menuPriter() 
+        { 
+            Console.WriteLine("Please select an option from the menu below: ");
+            Console.WriteLine("a. Input a Series. (Replace the current series)");
+            Console.WriteLine("b. Display the series in the order it was entered.");
+            Console.WriteLine("c. Display the series in the reversed order it was entered.");
+            Console.WriteLine("d. Display the series in sorted order (from low to high).");
+            Console.WriteLine("e. Display the Max value of the series.");
+            Console.WriteLine("f. Display the Min value of the series.");
+            Console.WriteLine("g. Display the Average of the series.");
+            Console.WriteLine("h. Display the Number of elements in the series.");
+            Console.WriteLine("i. Display the Sum of the series.");
+            Console.WriteLine("j. Exit");
+            return;
         }
         static int max(int[] intArr)
         {
@@ -49,13 +154,27 @@ namespace project_1
         static int[] revers(int[] intArr)
         {
             int[] reversedArr = new int[intArr.Length];
+            for (int i = 0; i < intArr.Length; i++)
+            {
+                reversedArr[i] = intArr[intArr.Length - 1 - i];
+            }
             return reversedArr;
         }
         static void arrPrinter(int[] intArr)
         {
+            Console.WriteLine("The series is: ");
+            foreach (int num in intArr)
+            {
+                Console.Write(num + " ");
+            }
+            Console.WriteLine();
+            return;
         }
-        static void singleNumPrinter(int[] intArr)
+        static void singleNumPrinter(int num)
         {
+            Console.WriteLine("The result is: ");
+            Console.WriteLine(num + " ");
+            return;
         }
     }
 }
